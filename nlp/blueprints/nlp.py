0661c10b4f00831fc51c13a1e184ac24eeec9cd8
MaBrur <mabruraas@gmail.com>
2019-08-14 16:30:51 +0200

    Added blueprint end point for NLP, and extended configuration with a kwargs dictionary

--- nlp/blueprints/nlp.py
+++ nlp/blueprints/nlp.py
@@ -1,23 +1,41 @@
 #! /usr/bin/env python3
 import logging
 import os
+import urllib
 
-from flask import Blueprint, current_app, request
+from flask import Blueprint, current_app
 from flask_cors import cross_origin
-from sqapi.api import responding
 
+from sqapi.api import responding
 
 log = logging.getLogger(__name__)
 bp = Blueprint(__name__, __name__, url_prefix='/nlp')
 
 
-@bp.route('/<text>', methods=['GET'])
+@bp.route('/text/<text>', methods=['GET'])
 @cross_origin()
-def get_all_nlp(text):
+def search_all_fields(text):
+    text = urllib.parse.unquote(text)
     db = get_database()
 
     log.info('Fetching all nlp')
-    result = db.fetch_document('nlp', {'multi_match': {'query': text, 'fields': ['*']}})
+    result = db.fetch_document('nlp', {'query': text, 'fields': ['*']}, 'multi_match')
+
+    if not result:
+        log.info('No entries found')
+        return responding.no_content(result)
+
+    log.debug('Entries: {}'.format(result))
+    return responding.ok([r.get('_source') for r in result])
+
+
+@bp.route('/uuid/<uuid_ref>', methods=['GET'])
+@cross_origin()
+def get_all_nlp(uuid_ref):
+    db = get_database()
+
+    log.info('Fetching all nlp')
+    result = db.fetch_document('nlp', {'uuid': uuid_ref}, 'match')
 
     if not result:
         log.info('No entries found')
